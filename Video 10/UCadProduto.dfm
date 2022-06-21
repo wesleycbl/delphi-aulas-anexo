66688dc6097e6740caa4422a7e04e31c74024c57
wesleycbl <wesleypsantos@hotmail.com>
2022-06-21 16:40:07 -0300

    Aula 10

---
+++ Video 10/UCadProduto.dfm
@@ -0,0 +1,217 @@
+object FCadProduto: TFCadProduto
+  Left = 0
+  Top = 0
+  Caption = 'Cadastro de Produto'
+  ClientHeight = 417
+  ClientWidth = 737
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnActivate = FormActivate
+  OnClose = FormClose
+  PixelsPerInch = 96
+  TextHeight = 13
+  object PDados: TPanel
+    Left = 8
+    Top = 8
+    Width = 721
+    Height = 177
+    Caption = 'PDados'
+    TabOrder = 0
+    object Label1: TLabel
+      Left = 24
+      Top = 24
+      Width = 33
+      Height = 13
+      Caption = 'C'#243'digo'
+    end
+    object Label2: TLabel
+      Left = 24
+      Top = 67
+      Width = 27
+      Height = 13
+      Caption = 'Nome'
+    end
+    object Label3: TLabel
+      Left = 24
+      Top = 113
+      Width = 39
+      Height = 13
+      Caption = 'Estoque'
+    end
+    object Label4: TLabel
+      Left = 121
+      Top = 113
+      Width = 24
+      Height = 13
+      Caption = 'Valor'
+    end
+    object edtCodProd: TEdit
+      Left = 24
+      Top = 40
+      Width = 121
+      Height = 21
+      TabOrder = 0
+    end
+    object edtNome: TEdit
+      Left = 24
+      Top = 86
+      Width = 248
+      Height = 21
+      TabOrder = 1
+    end
+    object edtEstoque: TEdit
+      Left = 24
+      Top = 132
+      Width = 73
+      Height = 21
+      TabOrder = 2
+    end
+    object edtValor: TEdit
+      Left = 121
+      Top = 132
+      Width = 73
+      Height = 21
+      TabOrder = 3
+    end
+    object DBEdit1: TDBEdit
+      Left = 576
+      Top = 21
+      Width = 134
+      Height = 21
+      DataField = 'CodProd'
+      DataSource = DSCLProduto
+      TabOrder = 4
+      Visible = False
+      OnChange = DBEdit1Change
+    end
+  end
+  object DBGrid1: TDBGrid
+    Left = 8
+    Top = 256
+    Width = 721
+    Height = 153
+    DataSource = DSCLProduto
+    TabOrder = 1
+    TitleFont.Charset = DEFAULT_CHARSET
+    TitleFont.Color = clWindowText
+    TitleFont.Height = -11
+    TitleFont.Name = 'Tahoma'
+    TitleFont.Style = []
+  end
+  object PControle: TPanel
+    Left = 8
+    Top = 199
+    Width = 721
+    Height = 41
+    TabOrder = 2
+    object btNovo: TButton
+      Left = 114
+      Top = 8
+      Width = 75
+      Height = 25
+      Caption = 'Novo'
+      TabOrder = 0
+      OnClick = btNovoClick
+    end
+    object btAlterar: TButton
+      Left = 195
+      Top = 8
+      Width = 75
+      Height = 25
+      Caption = 'Alterar'
+      TabOrder = 1
+      OnClick = btAlterarClick
+    end
+    object btRemover: TButton
+      Left = 276
+      Top = 8
+      Width = 75
+      Height = 25
+      Caption = 'Remover'
+      TabOrder = 2
+      OnClick = btRemoverClick
+    end
+    object btSalvar: TButton
+      Left = 365
+      Top = 8
+      Width = 75
+      Height = 25
+      Caption = 'Salvar'
+      TabOrder = 3
+      OnClick = btSalvarClick
+    end
+    object btCancelar: TButton
+      Left = 446
+      Top = 8
+      Width = 75
+      Height = 25
+      Caption = 'Cancelar'
+      TabOrder = 4
+      OnClick = btCancelarClick
+    end
+    object btSair: TButton
+      Left = 527
+      Top = 8
+      Width = 75
+      Height = 25
+      Caption = 'Sair'
+      TabOrder = 5
+      OnClick = btSairClick
+    end
+  end
+  object clProduto: TClientDataSet
+    PersistDataPacket.Data = {
+      6D0000009619E0BD0100000018000000040000000000030000006D0007436F64
+      50726F640400010000000000084E6F6D6550726F640100490000000100055749
+      4454480200020028000B4573746F71756550726F640400010000000000095661
+      6C6F7250726F6408000400000000000000}
+    Active = True
+    Aggregates = <>
+    FieldDefs = <
+      item
+        Name = 'CodProd'
+        DataType = ftInteger
+      end
+      item
+        Name = 'NomeProd'
+        DataType = ftString
+        Size = 40
+      end
+      item
+        Name = 'EstoqueProd'
+        DataType = ftInteger
+      end
+      item
+        Name = 'ValorProd'
+        DataType = ftFloat
+      end>
+    IndexDefs = <>
+    Params = <>
+    StoreDefs = True
+    Left = 664
+    Top = 104
+    object clProdutoCodProd: TIntegerField
+      FieldName = 'CodProd'
+    end
+    object clProdutoNomeProd: TStringField
+      FieldName = 'NomeProd'
+      Size = 40
+    end
+    object clProdutoEstoqueProd: TIntegerField
+      FieldName = 'EstoqueProd'
+    end
+    object clProdutoValorProd: TFloatField
+      FieldName = 'ValorProd'
+    end
+  end
+  object DSCLProduto: TDataSource
+    DataSet = clProduto
+    Left = 688
+    Top = 264
+  end
+end
---
+++ Video 10/UCadProduto.pas
@@ -0,0 +1,215 @@
+unit UCadProduto;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Vcl.Grids, Vcl.DBGrids,
+  Datasnap.DBClient, Vcl.ExtCtrls, Vcl.StdCtrls, Vcl.Mask, Vcl.DBCtrls;
+
+type
+  TFCadProduto = class(TForm)
+    PDados: TPanel;
+    clProduto: TClientDataSet;
+    DBGrid1: TDBGrid;
+    DSCLProduto: TDataSource;
+    clProdutoCodProd: TIntegerField;
+    clProdutoNomeProd: TStringField;
+    clProdutoEstoqueProd: TIntegerField;
+    clProdutoValorProd: TFloatField;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    PControle: TPanel;
+    btNovo: TButton;
+    btAlterar: TButton;
+    btRemover: TButton;
+    btSalvar: TButton;
+    btCancelar: TButton;
+    edtCodProd: TEdit;
+    edtNome: TEdit;
+    edtEstoque: TEdit;
+    edtValor: TEdit;
+    btSair: TButton;
+    DBEdit1: TDBEdit;
+    procedure FormActivate(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure btNovoClick(Sender: TObject);
+    procedure btAlterarClick(Sender: TObject);
+    procedure btRemoverClick(Sender: TObject);
+    procedure btSalvarClick(Sender: TObject);
+    procedure btCancelarClick(Sender: TObject);
+    procedure btSairClick(Sender: TObject);
+    procedure DBEdit1Change(Sender: TObject);
+  private
+    { Private declarations }
+    procedure mostra;
+    // comando CRTL + Shift + C
+    procedure limpar;
+    procedure controleBotoes(acao: integer = 0);
+  public
+    { Public declarations }
+  end;
+
+var
+  FCadProduto: TFCadProduto;
+  arquivo: string = 'C:\Projetos Anexo\Video 9\db\produto.xml';
+  acaogeral: integer = 3;
+
+implementation
+
+{$R *.dfm}
+
+procedure TFCadProduto.btAlterarClick(Sender: TObject);
+begin
+  acaogeral := 1;
+  controleBotoes(acaogeral);
+end;
+
+procedure TFCadProduto.btCancelarClick(Sender: TObject);
+begin
+  acaogeral := 3;
+  controleBotoes(acaogeral);
+end;
+
+procedure TFCadProduto.btNovoClick(Sender: TObject);
+begin
+  acaogeral := 0;
+
+  controleBotoes(acaogeral);
+end;
+
+procedure TFCadProduto.btRemoverClick(Sender: TObject);
+begin
+  clProduto.Delete;
+  acaogeral := 2;
+  controleBotoes(acaogeral);
+end;
+
+procedure TFCadProduto.btSairClick(Sender: TObject);
+begin
+  close;
+  acaogeral := 5;
+  controleBotoes(acaogeral);
+end;
+
+procedure TFCadProduto.btSalvarClick(Sender: TObject);
+begin
+
+  try
+    case acaogeral of
+      0:
+        begin
+          clProduto.Append;
+        end;
+      1:
+        begin
+          clProduto.Edit;
+        end;
+    end;
+
+    clProduto.FieldByName('CodProd').AsInteger :=
+      StrToIntDef(edtCodProd.Text, 0);
+
+    clProduto.FieldByName('NomeProd').AsString := edtNome.Text;
+
+    clProduto.FieldByName('EstoqueProd').AsInteger :=
+      StrToIntDef(edtEstoque.Text, 0);
+
+    clProduto.FieldByName('ValorProd').AsFloat :=
+      StrToFloatDef(edtValor.Text, 0);
+
+    clProduto.Post;
+
+    acaogeral := 3;
+    controleBotoes(3);
+  except
+      ShowMessage('Erro ao Salvar dados !');
+  end;
+
+end;
+
+procedure TFCadProduto.controleBotoes(acao: integer = 0);
+begin
+  btNovo.Enabled := false;
+  btAlterar.Enabled := false;
+  btRemover.Enabled := false;
+  btSalvar.Enabled := false;
+  btCancelar.Enabled := false;
+  btSair.Enabled := true;
+
+  case acao of
+    0:
+      begin
+        limpar();
+        btSalvar.Enabled := true;
+        btCancelar.Enabled := true;
+        if edtCodProd.CanFocus then
+          edtCodProd.SetFocus;
+      end;
+    1:
+      begin
+        btSalvar.Enabled := true;
+        btCancelar.Enabled := true;
+        if edtCodProd.CanFocus then
+          edtCodProd.SetFocus;
+
+      end;
+  else
+    begin
+      btNovo.Enabled := true;
+      if clProduto.RecordCount > 0 then
+      begin
+        btAlterar.Enabled := true;
+        btRemover.Enabled := true;
+      end;
+    end;
+  end;
+end;
+
+procedure TFCadProduto.DBEdit1Change(Sender: TObject);
+begin
+  if acaogeral in [0, 1] then
+    exit;
+  mostra;
+end;
+
+procedure TFCadProduto.FormActivate(Sender: TObject);
+begin
+  if FileExists(arquivo) then
+    clProduto.LoadFromFile(arquivo);
+
+end;
+
+procedure TFCadProduto.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  clProduto.SaveToFile(arquivo);
+end;
+
+procedure TFCadProduto.limpar;
+begin
+  edtCodProd.Text := '';
+  edtNome.Text := '';
+  edtEstoque.Text := '';
+  edtValor.Text := '';
+end;
+
+procedure TFCadProduto.mostra;
+begin
+
+  if (clProduto.Active = false) or (clProduto.RecordCount = 0) then
+  begin
+    limpar;
+    exit;
+  end;
+
+  edtCodProd.Text := IntToStr(clProduto.FieldByName('CodProd').AsInteger);
+  edtNome.Text := clProduto.FieldByName('NomeProd').AsString;
+  edtEstoque.Text := clProduto.FieldByName('EstoqueProd').AsString;
+  edtValor.Text := formatFloat('#0.00',
+    clProduto.FieldByName('ValorProd').AsFloat);
+end;
+
+end.
